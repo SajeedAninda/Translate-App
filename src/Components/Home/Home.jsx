eeac4ac01e772985a3266fa847159755452b2578
SajeedAninda <sajaninda@gmail.com>
2024-08-10 16:42:21 +0600

    Added logo

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import "./home.css"
+import logo from "../../assets/logo.svg"
 
 const Home = () => {
     return (
         <div className='h-[100vh] w-full homeBg'>
-            
+            <div className='flex justify-center items-center pt-20'>
+                <img className='w-[150px]' src={logo} alt="" />
+            </div>
         </div>
     );
 };
