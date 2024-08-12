5494ba14ed0434739ef0c82f1b9d021ea6922e23
SajeedAninda <sajaninda@gmail.com>
2024-08-12 16:30:37 +0600

    Created Active Buttons

--- src/Components/Translator/Translator.jsx
+++ src/Components/Translator/Translator.jsx
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Translator = () => {
+    let [activeButton, setActiveButton] = useState(null);
+
+    let handleButtonClick = (buttonName) => {
+        setActiveButton(buttonName);
+    }
+
     return (
         <div className='w-[90%] mx-auto py-20 flex justify-between items-center'>
-            <div className='h-[367px] bg-[#212936cc] p-8 rounded-2xl w-[50%] gap-6'>
-                <div className='flex gap-4'>
-                    <button className='text-[16px] text-[#4D5562] font-semibold cursor-pointer'>
+            <div className='h-[367px] bg-[#212936cc] p-8 rounded-3xl w-[50%] gap-6'>
+                <div className='flex gap-4 items-start justify-start'>
+                    <button onClick={() => handleButtonClick("Detect Language")} className='text-[16px] font-semibold cursor-pointer py-2 rounded-xl text-[#4D5562]'>
                         Detect Language
                     </button>
-                    <button className='text-[16px] text-[#4D5562] font-semibold cursor-pointer'>
+                    <button onClick={() => handleButtonClick("English")}  className={`text-[16px] font-semibold cursor-pointer px-3 py-2 rounded-xl ${
+                            activeButton === 'English' ? 'bg-[#4D5562] text-[#CDD5E0]' : 'text-[#4D5562] bg-transparent'
+                        }`}>
                         English
                     </button>
-                    <button className='text-[16px] text-[#4D5562] font-semibold cursor-pointer'>
+                    <button onClick={() => handleButtonClick("French")}  className={`text-[16px] font-semibold cursor-pointer px-3 py-2 rounded-xl ${
+                            activeButton === 'French' ? 'bg-[#4D5562] text-[#CDD5E0]' : 'text-[#4D5562] bg-transparent'
+                        }`}>
                         French
                     </button>
-                    <button className='text-[16px] text-[#4D5562] font-semibold cursor-pointer'>
+                    <button onClick={() => handleButtonClick("Spanish")}  className={`text-[16px] font-semibold cursor-pointer px-3 py-2 rounded-xl ${
+                            activeButton === 'Spanish' ? 'bg-[#4D5562] text-[#CDD5E0]' : 'text-[#4D5562] bg-transparent'
+                        }`}>
                         Spanish
                     </button>
                 </div>
+
+                <hr className="w-[100%] border border-[#4D5562] mt-4 mb-2 opacity-75" />
             </div>
         </div>
     );
