db1fb88353d4d6531b82488aaf7c8097a6e7f5f3
SajeedAninda <sajaninda@gmail.com>
2024-08-16 16:20:58 +0600

    Implemented real time translation from english to spanish

--- src/Components/Translator/Translator.jsx
+++ src/Components/Translator/Translator.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import soundLogo from "../../assets/sound_max_fill.svg";
 import copyLogo from "../../assets/Copy.svg";
 import translatorLogo from "../../assets/Sort_alfa.svg";
@@ -7,6 +7,9 @@ import exchangeIcon from "../../assets/Horizontal_top_left_main.svg";
 const Translator = () => {
     let [activeButton, setActiveButton] = useState(null);
     let [translationTextCount, setTranslationTextCount] = useState(0);
+    let [translationText, setTranslationText] = useState('Hello, how are you?');
+    let [translatedText, setTranslatedText] = useState("");
+    
 
     let handleButtonClick = (buttonName) => {
         setActiveButton(buttonName);
@@ -14,11 +17,23 @@ const Translator = () => {
 
     let handleTranslationText = (e) => {
         let translationText = e.target.value;
+        setTranslationText(translationText);
         setTranslationTextCount(translationText.length);
-        console.log(translationTextCount);
     }
 
+    useEffect(() => {
+        const translateText = async () => {
+            try {
+                const response = await fetch(`https://api.mymemory.translated.net/get?q=${encodeURIComponent(translationText)}&langpair=en|es`);
+                const data = await response.json();
+                setTranslatedText(data.responseData.translatedText);
+            } catch (error) {
+                console.error("Error translating text:", error);
+            }
+        };
 
+        translateText();
+    }, [translationText]);
 
     return (
         <div className='w-[90%] mx-auto py-16 flex flex-col lg:flex-row justify-between gap-6 items-center'>
@@ -44,7 +59,7 @@ const Translator = () => {
                 <hr className="w-[100%] border border-[#4D5562] mt-4 mb-2 opacity-75" />
 
                 <div className='relative'>
-                    <textarea onChange={handleTranslationText} className="bg-transparent resize-none outline-none text-white w-[100%]" name="translationText" id="translationText" cols={30} rows={6} maxLength={500}></textarea>
+                    <textarea onChange={handleTranslationText} value={translationText} className="bg-transparent resize-none outline-none text-white w-[100%]" name="translationText" id="translationText" cols={30} rows={6} maxLength={500}></textarea>
 
                     <p className='text-[#4D5562] text-[14px] absolute right-0 bottom-0'>{translationTextCount}/500</p>
                 </div>
@@ -55,15 +70,12 @@ const Translator = () => {
                         <img className='border-2 border-[#4D5562] rounded-xl p-2 cursor-pointer' src={copyLogo} alt="" />
                     </div>
 
-                    <buttons className='text-white px-4 py-3 rounded-xl border-2 bg-[#3662E3] border-[#7CA9F3] text-[16px] font-semibold flex gap-2 justify-between items-center hover:opacity-80 cursor-pointer'>
+                    <button onClick={() => { }} className='text-white px-4 py-3 rounded-xl border-2 bg-[#3662E3] border-[#7CA9F3] text-[16px] font-semibold flex gap-2 justify-between items-center hover:opacity-80 cursor-pointer'>
                         <img src={translatorLogo} alt="" />
                         Translate
-                    </buttons>
+                    </button>
                 </div>
             </div>
-
-
-
 
             <div className='h-fit md:h-[367px] bg-[#121826cc] p-8 rounded-3xl w-full lg:w-[50%] gap-6 secondDiv'>
                 <div className='flex justify-between items-center'>
@@ -88,7 +100,7 @@ const Translator = () => {
                 <hr className="w-[100%] border border-[#4D5562] mt-4 mb-2 opacity-75" />
 
                 <div className=''>
-                    <textarea onChange={handleTranslationText} className="bg-transparent resize-none outline-none text-white w-[100%]" name="translationText" id="translationText" cols={30} rows={6} maxLength={500}></textarea>
+                    <textarea value={translatedText} readOnly className="bg-transparent resize-none outline-none text-white w-[100%]" name="translationText" id="translationText" cols={30} rows={6} maxLength={500}></textarea>
                 </div>
 
                 <div className='flex mt-8'>
